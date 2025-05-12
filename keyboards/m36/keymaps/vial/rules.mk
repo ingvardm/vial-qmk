005bf4e745497daaf42ec4eec334b2d6d2c1c66b
igor poughtach <ingvardm@gmail.com>
2025-05-12 22:57:49 +0300

    initial

---
+++ keyboards/m36/keymaps/vial/rules.mk
@@ -0,0 +1,19 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+BOOTMAGIC_LITE_ENABLE = yes
+
+ENCODER_MAP_ENABLE  = no
+MOUSEKEY_ENABLE     = no
+EXTRAKEY_ENABLE = no
+OLED_ENABLE = no
+MUSIC_ENABLE        = no
+CONSOLE_ENABLE      = no
+COMMAND_ENABLE      = no
+KEY_OVERRIDE_ENABLE = no
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = yes
+VIALRGB_ENABLE      = yes
+COMBO_ENABLE        = yes
+
+QMK_SETTINGS        = yes

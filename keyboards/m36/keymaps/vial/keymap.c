01e0e5e5e55f0ddc5cb2d33dc1841c6d98e79023
igor poughtach <ingvardm@gmail.com>
2025-05-17 13:35:20 +0300

    wip

--- keyboards/m36/keymaps/vial/keymap.c
+++ keyboards/m36/keymaps/vial/keymap.c
@@ -1,10 +1,11 @@
 #include QMK_KEYBOARD_H
-#include "rgb_matrix.h"
+
+#include "quantum.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_split_3x5(
   //,----------------------------------------.  ,-----------------------------------------.
-      KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
+    KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,
   //|--------+--------+--------+--------+----|  |--------+--------+--------+--------+------|
       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
   //|--------+--------+--------+--------+----'  `--------+--------+--------+--------+--------|
@@ -51,3 +52,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                     //`--------------------------'  `--------------------------'
   )
 };
+
+#ifndef MAGIC_ENABLE
+uint16_t keycode_config(uint16_t keycode) {
+    return keycode;
+}
+#endif
+
+#ifndef MAGIC_ENABLE
+uint8_t mod_config(uint8_t mod) {
+    return mod;
+}
+#endif
--- keyboards/m36/keymaps/vial/rules.mk
+++ keyboards/m36/keymaps/vial/rules.mk
@@ -1,19 +1,20 @@
-VIA_ENABLE          = yes
-VIAL_ENABLE         = yes
-LTO_ENABLE          = yes
+VIA_ENABLE            = yes
+VIAL_ENABLE           = yes
+LTO_ENABLE            = yes
 BOOTMAGIC_LITE_ENABLE = yes
 
-ENCODER_MAP_ENABLE  = no
-MOUSEKEY_ENABLE     = no
-EXTRAKEY_ENABLE = no
-OLED_ENABLE = no
-MUSIC_ENABLE        = no
-CONSOLE_ENABLE      = no
-COMMAND_ENABLE      = no
-KEY_OVERRIDE_ENABLE = no
-RGBLIGHT_ENABLE     = no
-RGB_MATRIX_ENABLE   = yes
-VIALRGB_ENABLE      = yes
-COMBO_ENABLE        = yes
+CONSOLE_ENABLE        = no
+COMMAND_ENABLE        = no
+KEY_OVERRIDE_ENABLE   = no
+VIALRGB_ENABLE        = yes
+COMBO_ENABLE          = yes
+COMMAND_ENABLE        = no
+UNICODE_ENABLE        = no
+MOUSEKEY_ENABLE       = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+MUSIC_ENABLE = no
 
-QMK_SETTINGS        = yes
+QMK_SETTINGS          = yes
+
+VIAL_INSECURE = yes

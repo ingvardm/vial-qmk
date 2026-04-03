fa2bf9fe58f21b7867fed55f6f7062e0685d934f
ingvardm <ingvardm@gmail.com>
2026-04-03 15:00:07 +0300

    initial port

---
+++ keyboards/dh747/chloremipsum36/keymaps/vial/rules.mk
@@ -0,0 +1,17 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = yes
+
+CONSOLE_ENABLE        = no
+KEY_OVERRIDE_ENABLE   = no
+RGB_MATRIX_ENABLE     = yes
+VIALRGB_ENABLE        = yes
+COMBO_ENABLE          = yes
+COMMAND_ENABLE        = no
+UNICODE_ENABLE        = no
+MOUSEKEY_ENABLE       = no
+GRAVE_ESC_ENABLE      = no
+MAGIC_ENABLE          = no
+MUSIC_ENABLE          = no
+
+QMK_SETTINGS          = yes
